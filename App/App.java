93b0a06ad98eafd493d2d637e0a3588f4990a31f
Alex D <alexmm90@yandex.ru>
2025-03-14 10:00:42 +0300

    Core консольного приложения

---
+++ App/App.java
@@ -0,0 +1,35 @@
+package App;
+
+import Model.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class App {
+    private static List<Task> tasks = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Выберете задачу");
+            System.out.println(" " +
+                    "\n 1 - Добавить задачу" +
+                    "\n 2 - Показать список задач" +
+                    "\n 3 - Удалить задачу" +
+                    "\n 4 - Удалить все азадчи" +
+                    "\n 5 - Маркировать задачу как выполненную" +
+                    "\n 6 - Завершить программу");
+            int number = scanner.nextInt();
+            scanner.nextLine();
+            switch (number) {
+                case 6:
+                    System.out.println("Программа завершена");
+                    return;
+                default:
+                    System.out.println("Введён неверный индекс");
+
+            }
+        }
+    }
+}
---
+++ Model/Task.java
@@ -0,0 +1,31 @@
+package Model;
+
+import java.time.LocalDateTime;
+
+public class Task {
+    private String description;
+    private boolean completed;
+    private LocalDateTime addTime;
+
+    public Task(String description) {
+        this.description = description;
+        this.completed = false;
+        this.addTime = LocalDateTime.now();
+    }
+
+    public void makeCompleted() {
+        this.completed = true;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean getCompleted() {
+        return completed;
+    }
+
+    public LocalDateTime getAddTime() {
+        return addTime;
+    }
+}
