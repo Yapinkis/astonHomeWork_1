354e2ef75f2879b18b9ad3961699ac2f0982fdeb
Alex D <alexmm90@yandex.ru>
2025-03-17 11:45:48 +0300

    Добавлен метод удаления таска

--- App/App.java
+++ App/App.java
@@ -6,10 +6,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import static Service.DeleteTask.deleteTask;
+
 public class App {
-    private static List<Task> tasks = new ArrayList<>();
+    public static List<Task> tasks = new ArrayList<>();
 
     public static void main(String[] args) {
+        tasks.add(new Task("one"));
+        tasks.add(new Task("two"));
+        tasks.add(new Task("three"));
         Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.println("Выберете задачу");
@@ -23,6 +28,11 @@ public class App {
             int number = scanner.nextInt();
             scanner.nextLine();
             switch (number) {
+                case 3:
+                    System.out.println("Введите наименование задачи");
+                    String task = scanner.nextLine();
+                    deleteTask(task);
+                    break;
                 case 6:
                     System.out.println("Программа завершена");
                     return;
